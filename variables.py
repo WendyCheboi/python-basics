e7d0f9871c2d70e27fa7808bdda479ca25ce4a8d
Wendy Cheboi <cheboiwendy98@gmail.com>
2026-07-09 19:26:54 +0300

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,24 @@
+full_name = "Wendy Cheboi"
+job_title = "Data Quality Analyst"
+years_of_experience = 1
+is_learning_ml = True
+
+print(full_name)
+print(job_title)
+print(years_of_experience)
+print(is_learning_ml)
+
+print(f"My name is {full_name}.")
+print(f"I currently work as a {job_title}.")
+
+favorite_programming_language = "Python"
+dream_job = "Machine Learning Engineer"
+study_hours_per_week = 15
+enjoy_solving_problems = True
+
+print(
+    f"My favorite programming language is {favorite_programming_language}. "
+    f"My dream job is {dream_job}. "
+    f"I plan to study {study_hours_per_week} hours each week. "
+    f"Do I enjoy solving problems? {enjoy_solving_problems}."
+)
